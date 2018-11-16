c393f2470fab8ea4ec8178da5f570c234ec4800e
Lequn Chen <chenlequn22@gmail.com>
2018-11-16 10:20:23 -0800

    change scrape interval to 10s; disable altermanager&grafana;

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-# Create ConfigMap with prometheus rules for alerting
-kubectl --namespace monitoring create configmap --dry-run prometheus-rules \
-  --from-file=configs/prometheus/rules \
-  --output yaml \
-    > ./manifests/prometheus/prometheus-rules.yaml
-
-# Create ConfigMap for an external url
-kubectl --namespace monitoring create configmap --dry-run alertmanager-templates \
-  --from-file=configs/alertmanager-templates \
-  --output yaml \
-    > ./manifests/alertmanager/alertmanager-templates.yaml
-
-# Create ConfigMap with Grafana dashboards and datasources
-kubectl --namespace monitoring create configmap --dry-run grafana-import-dashboards \
-  --from-file=configs/grafana \
-  --output yaml \
-    > ./manifests/grafana/import-dashboards/configmap.yaml
+# # Create ConfigMap with prometheus rules for alerting
+# kubectl --namespace monitoring create configmap --dry-run prometheus-rules \
+#   --from-file=configs/prometheus/rules \
+#   --output yaml \
+#     > ./manifests/prometheus/prometheus-rules.yaml
+# 
+# # Create ConfigMap for an external url
+# kubectl --namespace monitoring create configmap --dry-run alertmanager-templates \
+#   --from-file=configs/alertmanager-templates \
+#   --output yaml \
+#     > ./manifests/alertmanager/alertmanager-templates.yaml
+# 
+# # Create ConfigMap with Grafana dashboards and datasources
+# kubectl --namespace monitoring create configmap --dry-run grafana-import-dashboards \
+#   --from-file=configs/grafana \
+#   --output yaml \
+#     > ./manifests/grafana/import-dashboards/configmap.yaml
 
 # Create ConfigMap with Prometheus config
 kubectl --namespace monitoring create configmap --dry-run prometheus-core \
